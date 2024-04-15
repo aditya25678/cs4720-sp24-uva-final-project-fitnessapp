4183c076c18614aed06d9dd870e70bc4c277ab88
Aditya Kumar <aditya25678@gmail.com>
2024-04-15 18:24:59 -0400

    Initial Commit

---
+++ FitnessMedia/FitnessMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessMediaApp.swift
+//  FitnessMedia
+//
+//  Created by Aditya Kumar on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
